561c6883af17b3e0396d4a2451764399a7b1c4cc
Logan <Logans1739@gmail.com>
2026-09-07 21:06:17 -0400

    session 2: load FRED TIPS data, convert dates

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("how are you", "what is your name")
+import pandas as pd
+df = pd.read_csv('https://fred.stlouisfed.org/graph/fredgraph.csv?id=DFII10')
+df.head(5)
+print(df.columns)
+print(df.dtypes)
+df['observation_date'] = pd.to_datetime(df['observation_date'])
+print(df.dtypes)
+
